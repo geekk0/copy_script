98a9a8c1a6827e0107fda2a9e666f5be6ef22083
gekk0 <gekk0dw@gmail.com>
2024-03-21 09:26:37 +0300

    mailing sh launcher, sync with prod

---
+++ run_mailing.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source /cloud/copy_script/cs_env/bin/activate
+
+# Navigate to the directory containing the Python script
+cd /cloud/copy_script
+
+# Run the Python script
+python mailing.py
+
+# Deactivate the virtual environment
+deactivate
+
+
+
+# 0 10 * * * /path/to/run_mailer
+# 30 9 * * * /bin/bash /cloud/copy_script/test_cron.sh >> /cloud/copy_script/test_logfile.log 2>&1
