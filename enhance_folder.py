efc48a93ba8cfdc74c66b52f4e919af7bc8f424a
gekk0 <gekk0dw@gmail.com>
2024-05-26 14:35:07 +0300

    image enhancer, script enhance specific folder v2

--- enhance_folder.py
+++ enhance_folder.py
@@ -9,3 +9,6 @@ settings_file_path = os.path.join('/cloud/copy_script', settings_file)
 
 enhancer = ImageEnhancer(read_settings_file(settings_file_path))
 enhancer.enhance_folder(folder_path)
+enhancer.chown_folder(folder_path)
+enhancer.index_folder(folder_path)
+
