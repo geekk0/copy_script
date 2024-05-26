9880f0480dc7901c0663e4ab3f6e034f88d18543
gekk0 <gekk0dw@gmail.com>
2024-05-26 14:25:07 +0300

    image enhancer, script enhance specific folder

---
+++ enhance_folder.py
@@ -0,0 +1,11 @@
+import os
+
+from image_enhancer import ImageEnhancer, read_settings_file
+
+settings_file = input("Enter settings file: ")
+folder_path = input("Enter folder path: ")
+
+settings_file_path = os.path.join('/cloud/copy_script', settings_file)
+
+enhancer = ImageEnhancer(read_settings_file(settings_file_path))
+enhancer.enhance_folder(folder_path)
