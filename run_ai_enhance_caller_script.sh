1aa8ffec70f3f0dd7d514f53a1c10dee85e72a72
gekk0 <gekk0dw@gmail.com>
2024-08-02 00:21:22 +0300

    enhance_caller, create service file

---
+++ run_ai_enhance_caller_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source /cloud/copy_script/cs_env/bin/activate
+
+# Navigate to the directory containing the Python script
+cd /cloud/copy_script
+
+# Run the Python script
+python enhance_caller.py
