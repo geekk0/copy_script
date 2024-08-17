81c32eb260bafeb1aa2063637f5ff39bb38d8a3b
Gekk0 <gekk0dw@gmail.com>
2024-08-17 20:17:13 +0300

    bot, call enhance_folder rs v3

--- tg_bot_aio/bot/handlers/handler_commands.py
+++ tg_bot_aio/bot/handlers/handler_commands.py
@@ -5,7 +5,7 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 
-from ..keyboards import create_kb
+from ..keyboards import create_kb, modes_kb
 from ..service import studio_path, studio_names, mode_names
 from ..middleware import ChatIDChecker
 from ..bot_setup import logger
@@ -16,8 +16,7 @@ command_router = Router()
 @command_router.message(CommandStart())
 async def handle_start_command(message: Message, state: FSMContext):
     await state.clear()
-    kb = await create_kb(mode_names, mode_names * len(mode_names))
-    await message.answer(text="Выберите действие:", reply_markup=kb)
+    await message.answer(text="Выберите действие:", reply_markup=modes_kb)
 
 
 command_router.message.middleware(ChatIDChecker())
--- tg_bot_aio/bot/keyboards.py
+++ tg_bot_aio/bot/keyboards.py
@@ -13,6 +13,17 @@ async def create_kb(button_labels, callback_data):
         keyboard.append(row)
     return InlineKeyboardMarkup(inline_keyboard=keyboard)
 
+modes_kb = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text='Индексация', callback_data='Индексация'),
+        InlineKeyboardButton(text='Обработка', callback_data='Обработка'),
+        InlineKeyboardButton(text='Рассылка', callback_data='Рассылка'),
+        InlineKeyboardButton(text='Рассылка', callback_data='ИИ Обработка'),
+
+    ],
+
+])
+
 enhance_rs_kb = InlineKeyboardMarkup(inline_keyboard=[
     [
         InlineKeyboardButton(text='Запустить', callback_data='Обработка:запустить'),
