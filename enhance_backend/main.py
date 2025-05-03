625e37cf503a3f8b98ac81e80a3fdae94dc7cbc9
Gekk0 <gekk0dw@gmail.com>
2025-05-03 14:42:41 +0300

    enhance back logger to utils v5

--- enhance_backend/main.py
+++ enhance_backend/main.py
@@ -2,6 +2,9 @@ import os
 from typing import AsyncContextManager
 
 from fastapi import FastAPI
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import StreamingResponse
 from tortoise import Tortoise
 from os import environ
 from dotenv import load_dotenv
@@ -10,6 +13,7 @@ from enhance_backend.database.db_config import DATABASE_URL
 from enhance_backend.routers.clients import clients_router
 from enhance_backend.routers.tasks import tasks_router
 from enhance_backend.routers.packages import packages_router
+from enhance_backend.utils import logger
 
 load_dotenv()
 
@@ -22,6 +26,27 @@ async def lifespan(application: FastAPI) -> AsyncContextManager[None]:
     await shutdown()
 
 app = FastAPI(lifespan=lifespan)
+
+
+class RequestLoggerMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        logger.info(f"Запрос: {request.method} {request.url} {request.headers}")
+
+        response = await call_next(request)
+
+        #  ответа
+        if isinstance(response, StreamingResponse):
+            logger.info(f"Ответ (streaming): {response.status_code} {response.headers}")
+        else:
+            try:
+                logger.info(f"Ответ: {response.status_code} {response.body[:100]}")  # Логируем только часть тела
+            except AttributeError:
+                logger.info(f"Ответ: {response.status_code} (без тела)")
+
+        return response
+
+
+app.add_middleware(RequestLoggerMiddleware)
 
 
 async def init_db():
@@ -46,5 +71,4 @@ if not os.path.exists(log_folder):
 if __name__ == "__main__":
     import uvicorn
     backend_port = environ.get("BACKEND_PORT")
-    access_log = True
-    uvicorn.run(app, host="0.0.0.0", port=int(backend_port))
+    uvicorn.run(app, host="0.0.0.0", port=int(backend_port), access_log=True)
--- enhance_backend/utils.py
+++ enhance_backend/utils.py
@@ -1,6 +1,4 @@
 import os
-import logging
-
 from loguru import logger
 
 log_folder = os.path.join(os.getcwd(), "logs")
@@ -9,23 +7,8 @@ if not os.path.exists(log_folder):
 
 logger.add(
     os.path.join(log_folder, "enhance_backend.log"),
-    format="{time} {level} {message}",
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
     rotation="10 MB",
     compression="zip",
     level="DEBUG"
 )
-
-
-class InterceptHandler(logging.Handler):
-    def emit(self, record):
-        try:
-            level = logger.level(record.levelname).name
-        except ValueError:
-            level = record.levelno
-        logger.log(level, record.getMessage())
-
-
-logging.basicConfig(handlers=[InterceptHandler()], level=0)
-for logger_name in ("uvicorn", "uvicorn.access", "uvicorn.error", "fastapi"):
-    logging.getLogger(logger_name).handlers = [InterceptHandler()]
-    logging.getLogger(logger_name).propagate = False
