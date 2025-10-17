fb28192cfbd942e0411ef33363a7ad40ca72b809
Oleg <gekk0dw@gmail.com>
2025-10-17 15:21:00 +0300

    import client.api_manager fix

--- tg_bot_aio/bot/modes/queues.py
+++ tg_bot_aio/bot/modes/queues.py
@@ -6,7 +6,6 @@ from aiogram.types import Message, ChatPhoto
 from aiogram.filters import Command
 from aiogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
-from clients_bot.modes.select_files import api_manager
 from clients_bot.enhance_backend_api import EnhanceBackendAPI
 from ..bot_setup import form_router, logger
 from ..keyboards import create_kb
